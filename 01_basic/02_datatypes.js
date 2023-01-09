48a7341994b57906b6db8ef1d647d902950c6660
Tohidujjaman Hoque <80402614+tj8016@users.noreply.github.com>
2023-01-09 04:59:25 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // We are using node js, not browser
+
+console.log(3 
+    + 3) // code readability should be high
+
+
+let name = "Tohidujjaman Hoque";
+let age = 21;
+let isLoggedIn = false;
+
+// Premative Datatypes
+
+// number => 2 to power 53
+// bigint =>
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => value is not assign
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // undefined
+console.log(typeof null); // object
